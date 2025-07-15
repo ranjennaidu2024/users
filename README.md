8413c18fedf70b862f648e89ce28d6b6be3b90f8
ranjennaidu2024 <ranjennaidu2022@gmail.com>
2025-07-15 11:13:57 +0800

    Initial commit of spring reactive project with embedded h2 database

---
+++ README.md
@@ -0,0 +1,9 @@
+1. https://start.spring.io/
+For creation using the above , maven, java 17 , jar , 3.5.3
+Add the following dependencies:
+   - Spring Reactive Web
+   - H2 database
+   - Spring Data R2DBC
+
+2Configured and tested connection to H2 Database in `application.properties`:
+```properties
---
+++ src/main/java/com/ranjen/reactive/ws/users/infrastructure/H2ConsoleConfiguration.java
@@ -0,0 +1,35 @@
+package com.ranjen.reactive.ws.users.infrastructure;
+
+import org.h2.tools.Server;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.EventListener;
+
+import java.sql.SQLException;
+
+//to test
+// localhost:8082/h2-console
+// JDBC URL in console change to: jdbc:h2:mem:testDb
+// enter username and password configured in application.properties
+
+@Configuration
+//@Profile({"dev","test"})
+@Profile("!prod & !production")
+public class H2ConsoleConfiguration {
+
+    private Server webServer;
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void start() throws SQLException {
+        String WEB_PORT = "8082";
+        this.webServer = Server.createWebServer("-webPort", WEB_PORT).start();
+    }
+
+    @EventListener(ContextClosedEvent.class)
+    public void stop() {
+        this.webServer.stop();
+    }
+
+}
