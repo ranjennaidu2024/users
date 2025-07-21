53660026151bfe5b1b142a4608a12a6d16f73f8e
ranjennaidu2024 <ranjennaidu2022@gmail.com>
2025-07-21 15:06:33 +0800

    25. Mono: Returning a User object in HTTP Response body

--- src/main/java/com/ranjen/reactive/ws/users/presentation/UserController.java
+++ src/main/java/com/ranjen/reactive/ws/users/presentation/UserController.java
@@ -4,13 +4,24 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/users") //   http://localhost:8080/users
 public class UserController {
 
+//    @PostMapping
+//    public Mono<String> createUser(@RequestBody @Valid Mono<CreateUserRequest> createUserRequest) {
+//        return Mono.just("OK");
+//    }
+
     @PostMapping
-    public Mono<String> createUser(@RequestBody @Valid Mono<CreateUserRequest> createUserRequest) {
-        return Mono.just("OK");
+    public Mono<UserRest> createUser(@RequestBody @Valid Mono<CreateUserRequest> createUserRequest) {
+        return createUserRequest.map(request -> new UserRest(UUID.randomUUID(),
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail())
+        );
     }
 }
